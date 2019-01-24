4f814652ee176f030c76a7c32d5d5cd085e88d83
Daniel Laguna <daniel.laguna.soto@accenture.com>
2019-01-24 18:42:55 +0100

    Modified load_classes method. Now it loads all the classes although there is no carriage return

--- utils/utils.py
+++ utils/utils.py
@@ -15,9 +15,8 @@ def load_classes(path):
     """
     Loads class labels at 'path'
     """
-    fp = open(path, "r")
-    names = fp.read().split("\n")[:-1]
-    return names
+    with open(path, 'r') as f:
+        return f.read().splitlines()
 
 
 def weights_init_normal(m):
